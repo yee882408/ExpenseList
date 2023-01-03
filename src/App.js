81e7d2ccdc4252cb841c53ad3bc1d181e030e9e2
yee882408 <saw634703@gmail.com>
2023-01-03 17:24:02 +0800

    Ver.6

--- src/App.js
+++ src/App.js
@@ -1,27 +1,27 @@
-import React, { useState } from 'react';
+import React, { useState } from "react";
 
-import NewExpense from './components/NewExpense/NewExpense';
-import Expenses from './components/Expenses/Expenses';
+import NewExpense from "./components/NewExpense/NewExpense";
+import Expenses from "./components/Expenses/Expenses";
 
 const DUMMY_EXPENSES = [
   {
-    id: 'e1',
-    title: 'Toilet Paper',
-    amount: 94.12,
-    date: new Date(2020, 7, 14),
+    id: "e1",
+    title: "新電視",
+    amount: 800,
+    date: new Date(2022, 7, 14),
   },
-  { id: 'e2', title: 'New TV', amount: 799.49, date: new Date(2021, 2, 12) },
+  { id: "e2", title: "衣櫃", amount: 50, date: new Date(2022, 2, 12) },
   {
-    id: 'e3',
-    title: 'Car Insurance',
-    amount: 294.67,
-    date: new Date(2021, 2, 28),
+    id: "e3",
+    title: "壽險",
+    amount: 60,
+    date: new Date(2022, 2, 28),
   },
   {
-    id: 'e4',
-    title: 'New Desk (Wooden)',
-    amount: 450,
-    date: new Date(2021, 5, 12),
+    id: "e4",
+    title: "新電腦",
+    amount: 1000,
+    date: new Date(2022, 5, 12),
   },
 ];
 
